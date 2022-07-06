41f5c5c26186757122f6ac19ca6ee5fd87616d74
pgmcr <ph.gilde@gmail.com>
2022-07-06 15:24:58 +0200

    first commit

---
+++ app/__init__.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from app.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
+
+project_name = ""
+app = Flask(__name__)
+app.config.from_object(Config)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db, compare_type=True)
+
+from app import models, routes
+
+if not app.debug:
+    if app.config["MAIL_SERVER"]:
+        auth = None
+        if app.config["MAIL_USERNAME"] or app.config["MAIL_PASSWORD"]:
+            auth = (app.config["MAIL_USERNAME"], app.config["MAIL_PASSWORD"])
+        secure = None
+        if app.config["MAIL_USE_TLS"]:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(app.config["MAIL_SERVER"], app.config["MAIL_PORT"]),
+            fromaddr="logging@" + app.config["MAIL_SERVER"],
+            toaddrs=app.config["ADMINS"],
+            subject="filessafe Fehler",
+            credentials=auth,
+            secure=secure,
+        )
+        mail_handler.set_level(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler("logs/filessafe.log", maxBytes=10240, backupCount=10)
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+    app.logger.info("filessafe starting")
